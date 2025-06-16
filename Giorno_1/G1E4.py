5eead99c881b816092d7596452a8753b52066ee2
LorenzoMartemucci <Lorenzo.Martemucci@it.ey.com>
2025-06-16 16:02:18 +0200

    refactor: sistemato file G1E4.py

--- Giorno_1/G1E4.py
+++ Giorno_1/G1E4.py
@@ -1,49 +1,32 @@
-# Il codice sporco da ripulire
-
-global_value = 0 # variabile globale usata a caso
-
-def data(x, y, z):
-    temp = []
-    # codice morto
-    # print("Questo era un vecchio debug")
-
-    for i in range(len(x)):
-        if x[i] > 10:
-            temp.append(x[i] + y[i] + z)
-        else:
-            if y[i] > 5:
-                temp.append(x[i] * 2 + y[i])
-            else:
-                temp.append(0)
-    for i in range(len(x)):
-        if x[i] > 10:
-            temp.append(x[i] + y[i] + z)
-        else:
-            if y[i] > 5:
-                temp.append(x[i] * 2 + y[i])
-            else:
-                temp.append(0)
-
-    for i in range(len(x)):
-        if temp[i] > 50:
+#Codice pulito
+def analizza_valori_lista(x, y, z):
+    temp = genera_lista_combinata(x, y, z)
+    for val in temp:
+        if val > 50:
             print("Grande valore trovato!")
+        elif val == 0:
+            print("Zero trovato!")
         else:
-            if temp[i] == 0:
-                print("Zero trovato!")
-            else:
-                print("Valore: ", temp[i])
-
-    useless = 5  # parametro inutilizzato
+            print("Valore:", val)
     return temp
 
-def foo(a, b, c, d):
-    if a > 0:
-        if b > 0:
-            if c > 0:
-                print("Tutti positivi")
-    return a+b+c+d
+def genera_lista_combinata(x, y, z):
+    temp = []
+    for i in range(len(x)):
+        if x[i] > 10:
+            temp.append(x[i] + y[i] + z)
+        elif y[i] > 5:
+            temp.append(x[i] * 2 + y[i])
+        else:
+            temp.append(0)
+    return temp
 
-def funzione_che_esamina_i_numeri(lista):
+def somma_e_controlla_positivi(a, b, c, d):
+    if a > 0 and b > 0 and c > 0 and d > 0:
+        print("Tutti positivi.")
+    return a + b + c + d
+
+def analizza_e_stampa_info_numeri(lista):
     total = 0
     for el in lista:
         if el > 0:
@@ -60,13 +43,7 @@ def funzione_che_esamina_i_numeri(lista):
     for el in lista:
         if el > 100:
             print("Molto grande:", el)
-    # codice morto
-    # return 123
     result = total
     return result
-
-# codice inutilizzato
-# def mai_usata():
-#     pass
 
 print("Fine programma")
