58b0a9a69265323c9cbd9cf8081c9f88455fc04a
AlessandroSaiu <alessandro.antonio.saiu@it.ey.com>
2025-06-23 10:30:52 +0200

    Revisione esercizio 5

--- Giorno_5/G5E1_Azure_OpenAI.py
+++ Giorno_5/G5E1_Azure_OpenAI.py
@@ -7,9 +7,9 @@ content += "\nQual è il titolo azionario che è stato acquistato di più?"
 
 # Crea il client Azure OpenAI
 client = openai.AzureOpenAI(
-    api_key=
-    azure_endpoint=
-    api_version=
+    api_key="your_api_key_here",  
+    azure_endpoint="your_azure_endpoint_here",  
+    api_version="your_api_version_here"  
 )
 
 response = client.chat.completions.create(
