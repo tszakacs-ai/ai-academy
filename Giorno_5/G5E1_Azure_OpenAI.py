27ebb726b8842f176c2708bf0022759be534592b
Joanna Ben Kakitie <joanna.ben.kakitie@it.ey.com>
2025-06-24 08:56:25 +0200

    changes exercise Giorno_2

--- Giorno_5/G5E1_Azure_OpenAI.py
+++ Giorno_5/G5E1_Azure_OpenAI.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import openai
 
 # Crea il client Azure OpenAI
@@ -17,4 +18,25 @@ response = client.chat.completions.create(
     temperature=1,
 )
 
+=======
+import openai
+
+# Crea il client Azure OpenAI
+client = openai.AzureOpenAI(
+    api_key="LA_TUA_API_KEY",  # <-- La tua chiave API di Azure OpenAI
+    azure_endpoint="IL_TUO_API_ENDPOINT", # <-- Il tuo  API endpoint di Azure OpenAI
+    api_version="2024-12-01-preview",  # <-- La versione dal portale Azure
+)
+
+response = client.chat.completions.create(
+    model="o4-mini",  # <-- Il nome esatto del deployment in Azure
+    messages=[
+        {"role": "system", "content": "Sei un assistente AI."},
+        {"role": "user", "content": "Qual è la capitale dell'Italia?"}
+    ],
+    max_completion_tokens=256,
+    temperature=1,
+)
+
+>>>>>>> features/joanna-benkakitie
 print(response.choices[0].message.content)
