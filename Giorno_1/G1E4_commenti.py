ca3777ffa5013861601030e3ccdf6adb046cd361
Andrea Pitzus <andrea.pitzus@it.ey.com>
2025-06-25 18:26:05 +0200

    refactor: update the code using clean code rules

--- Giorno_1/G1E4_commenti.py
+++ Giorno_1/G1E4_commenti.py
@@ -1,104 +1,7 @@
 # ESEMPIO DI CODICE "SPORCO" DA RIPULIRE
 # (ogni punto evidenzia un problema visto in aula e quale tecnica va usata per correggerlo)
 
-global_value = 0  # [DA EVITARE] Variabile globale non necessaria (preferire variabili locali o parametri)
-
-def data(x, y, z):
-    # [CODE SMELL: NOME GENERICO]
-    # Rinominare la funzione con un nome chiaro che spiega cosa fa
-    temp = []
-    
-    # [CODICE MORTO]
-    # Eliminare o sostituire print di debug/commentate
-    # print("Questo era un vecchio debug")
-    
-    # [CODE SMELL: DUPLICAZIONE]
-    # Il ciclo seguente è duplicato (stessa logica due volte di fila). Estrarre in una funzione separata.
-    for i in range(len(x)):
-        # [STRUTTURE ANNIDATE]
-        # Troppa annidamento. Ristrutturare usando funzioni e if più piatti.
-        if x[i] > 10:
-            temp.append(x[i] + y[i] + z)
-        else:
-            if y[i] > 5:
-                temp.append(x[i] * 2 + y[i])
-            else:
-                temp.append(0)
-    for i in range(len(x)):
-        if x[i] > 10:
-            temp.append(x[i] + y[i] + z)
-        else:
-            if y[i] > 5:
-                temp.append(x[i] * 2 + y[i])
-            else:
-                temp.append(0)
-    
-    # [CODE SMELL: LOGICA ANNIDATA E STAMPA DIRETTA]
-    # Separare la logica di controllo e la stampa in funzioni dedicate, evitare stampe in funzioni "core"
-    for i in range(len(x)):
-        if temp[i] > 50:
-            print("Grande valore trovato!")  # Preferire return/log rispetto a print diretto
-        else:
-            if temp[i] == 0:
-                print("Zero trovato!")
-            else:
-                print("Valore: ", temp[i])
-    
-    useless = 5  # [PARAMETRO/VARIABILE INUTILE] - da eliminare
-    
-    return temp
-
-def foo(a, b, c, d):
-    # [NOME GENERICO, STRUTTURE ANNIDATE]
-    # Rinominare la funzione in modo significativo e ridurre l'annidamento.
-    if a > 0:
-        if b > 0:
-            if c > 0:
-                print("Tutti positivi")
-    return a+b+c+d
-
-def funzione_lunghissima(lista):
-    # [FUNZIONE TROPPO LUNGA]
-    # Spezzare questa funzione in più sotto-funzioni
-    total = 0
-    for el in lista:
-        if el > 0:
-            total += el
-        else:
-            total -= el
-    for el in lista:
-        if el % 2 == 0:
-            print("Pari:", el)
-        else:
-            print("Dispari:", el)
-    for el in lista:
-        print("Elemento:", el)
-    for el in lista:
-        if el > 100:
-            print("Molto grande:", el)
-    # [CODICE MORTO]
-    # Eliminare codice non usato
-    # return 123
-    result = total
-    return result
-
-# [CODICE INUTILE/NON USATO]
-# Definire solo funzioni effettivamente utilizzate nel progetto
-# def mai_usata():
-#     pass
-
-
-"""
-Esempio di refactoring di una code-base "sporca":
-- Eliminazione variabili globali
-- Uso di nomi chiari e funzioni brevi
-- Rimozione duplicazione e codice morto
-- Semplificazione strutture annidate
-- Separazione logica/calcolo da input/output
-- Docstring NumPy-style
-"""
-
-def calcola_valori(lista_x, lista_y, incremento):
+def preprocessing(lista_x, lista_y, incremento):
     """
     Calcola una lista di valori in base a regole definite sugli elementi di lista_x e lista_y.
 
@@ -127,14 +30,14 @@ def calcola_valori(lista_x, lista_y, incremento):
         risultati.append(risultato)
     return risultati
 
-def stampa_analisi(valori):
+def controllo_valori(valori):
     """
     Analizza e stampa messaggi specifici sui valori calcolati.
 
     Parameters
     ----------
     valori : list of int or float
-        Lista di valori su cui effettuare l’analisi.
+        Lista di valori su cui effettuare l'analisi.
 
     Returns
     -------
@@ -148,7 +51,7 @@ def stampa_analisi(valori):
         else:
             print(f"Valore: {valore}")
 
-def tutti_positivi(*args):
+def controllo_valori_positivi(*args):
     """
     Verifica se tutti i valori forniti sono positivi.
 
@@ -186,9 +89,9 @@ def calcola_totale(lista):
             totale -= el
     return totale
 
-def stampa_report(lista):
+def controllo_pari_dispari(lista):
     """
-    Stampa varie analisi sugli elementi della lista.
+    Stampa se gli elementi della lista sono pari o dispari.
 
     Parameters
     ----------
@@ -204,30 +107,74 @@ def stampa_report(lista):
             print("Pari:", el)
         else:
             print("Dispari:", el)
+
+def visualizza_elementi(lista):
+    """
+    Stampa tutti gli elementi della lista.
+
+    Parameters
+    ----------
+    lista : list of int or float
+        Lista di valori.
+
+    Returns
+    -------
+    None
+    """
     for el in lista:
         print("Elemento:", el)
+
+def visualizza_grandi_valori(lista, soglia=100):
+    """
+    Stampa i valori della lista che superano una soglia.
+
+    Parameters
+    ----------
+    lista : list of int or float
+        Lista di valori.
+    soglia : int or float, optional
+        Valore sopra il quale considerare un elemento grande (default 100).
+
+    Returns
+    -------
+    None
+    """
     for el in lista:
-        if el > 100:
+        if el > soglia:
             print("Molto grande:", el)
 
-# Mancano dati esempi e l'avvio del programma
+def stampa_report_completo(lista):
+    """
+    Stampa un report completo sulla lista.
+
+    Parameters
+    ----------
+    lista : list of int or float
+        Lista di valori.
+
+    Returns
+    -------
+    None
+    """
+    controllo_pari_dispari(lista)
+    visualizza_elementi(lista)
+    visualizza_grandi_valori(lista)
 
 def main():
-    # Esempio di esecuzione del programma completo
+    """
+    Funzione principale del programma.
+    """
     x = [5, 12, 8, 15]
     y = [6, 4, 7, 2]
     incremento = 10
 
-    valori = calcola_valori(x, y, incremento)  # Logica principale, funzione breve e chiara
-    stampa_analisi(valori)                    # Separazione output
-    print("Tutti positivi?", tutti_positivi(*x, *y, incremento))
+    valori = preprocessing(x, y, incremento)
+    controllo_valori(valori)
+    print("Tutti positivi?", controllo_valori_positivi(*x, *y, incremento))
     totale = calcola_totale(valori)
     print("Totale:", totale)
-    stampa_report(valori)
+    stampa_report_completo(valori)
+    print("Fine programma")
 
 if __name__ == "__main__":
     main()
-
-
-print("Fine programma")  # [STAMPA NON MOTIVATA] – da eliminare o spostare in main/log
-
